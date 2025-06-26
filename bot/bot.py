e63c6b3743976b634f6288a6033a3b971d9432ff
kravchend <kravchend@gmail.com>
2025-06-26 20:16:04 +0200

    Изменения в файле bot.py и gitignore

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+# Загрузи переменные окружения до импорта Django!
+dotenv_path = ".env.local" if os.path.exists(".env.local") else ".env"
+load_dotenv(dotenv_path)
+
 import asyncio
 import logging
-import os
 import django
+
+# Теперь переменные, такие как DATABASE_HOST, уже в окружении
+print("DATABASE_HOST из окружения:", os.environ.get("DATABASE_HOST"))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TelegramBot_Calendar.settings")
 django.setup()
