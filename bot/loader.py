761134a1a836928574531eddeeaf2b9fa318b570
kravchend <kravchend@gmail.com>
2025-06-26 19:15:07 +0200

    Внесены изменения: .env.local добавлен в .gitignore, обновлён loader.py, добавлены Docker-файлы

--- bot/loader.py
+++ bot/loader.py
@@ -2,8 +2,13 @@ import os
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenv_path = ".env.local" if os.path.exists(".env.local") else ".env"
+load_dotenv(dotenv_path)
+
 API_TOKEN = os.getenv("BOT_TOKEN")
+if not API_TOKEN:
+    raise RuntimeError("BOT_TOKEN is not set in environment variables!")
+
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot=bot)
 
