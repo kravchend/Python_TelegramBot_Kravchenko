e204997c1ed7ca2c0e60722e8b6d0b50d81fcf3f
kravchend <kravchend@gmail.com>
2025-06-27 14:11:28 +0200

    Подготовка к упаковке

--- TelegramBot_Calendar/settings.py
+++ TelegramBot_Calendar/settings.py
@@ -14,10 +14,15 @@ from dotenv import load_dotenv
 
 from pathlib import Path
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_path = BASE_DIR / ".env.local"
+if dotenv_path.exists():
+    load_dotenv(dotenv_path)
+else:
+    load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -85,7 +90,7 @@ DATABASES = {
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+        'PORT': os.getenv('DATABASE_PORT', 5432),
     }
 }
 
